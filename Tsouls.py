cf8778189a89c359fa0e64eb7ae49496f1c3286a
YanPek <54765921+YanPek@users.noreply.github.com>
2020-01-20 02:20:50 +0300

    Updatings
    •CAM-HACKERS
    •metasploit-framework
    •PhoneInfoga
    •SayHello

--- Tsouls.py
+++ Tsouls.py
@@ -23,6 +23,16 @@ print(Fore.YELLOW + ''' ________    _____     ____     __    __   _____        _
 03) Instagram + cupp (Брут паролей инстаграм + создание словаря)
 
 04)termux-ngrok (создание публичных url)
+
+05)CAM-HACKERS (Получение ip различных камер)
+
+06)metasploit-framework (Тут я в двух словах не обьясню, придется гуглит)
+
+ps. В случае ошибков порыскайте в инете
+
+07)PhoneInfoga (Информация о владельце номера)
+
+8)SayHello (Заполучение снимка лица человека находившегося в вашей точке доступа)
 ''')
 
 choose = int(input(Fore.YELLOW+"Выберите число: "+Style.RESET_ALL))
@@ -37,6 +47,19 @@ if choose == 0:
 	os.system("git clone https://github.com/Mebus/cupp.git")
 	#4
 	os.system("git clone https://github.com/tchelospy/termux-ngrok.git")
+	#5
+	os.system("git clone https://github.com/AngelSecurityTeam/Cam-Hackers")
+	#6
+	os.system("pkg install root-repo")
+	os.system("pkg install perl")
+	os.system("pkg install ruby")
+	os.system("apt update && apt upgrade")
+	os.system("pkg uodate && pkg upgrade")
+	os.system("pkg install metasploit")
+	#7
+	os.system("git clone https://github.com/sundowndev/PhoneInfoga")
+	#8
+	os.system("git clone https://github.com/thelinuxchoice/saycheese")
 
 elif choose == 1:
 	os.system("apt install nmap")
@@ -50,5 +73,23 @@ elif choose == 3:
 
 elif choose == 4:
 	os.system("git clone https://github.com/tchelospy/termux-ngrok.git")
+
+elif choose == 5:
+	os.system("git clone https://github.com/AngelSecurityTeam/Cam-Hackers")
+
+elif choose == 6:
+	os.system("pkg install root-repo")
+	os.system("pkg install perl")
+	os.system("pkg install ruby")
+	os.system("apt update && apt upgrade")
+	os.system("pkg uodate && pkg upgrade")
+	os.system("pkg install metasploit")
+	
+elif choose == 7:
+	os.system("git clone https://github.com/sundowndev/PhoneInfoga")
+
+elif choose == 8:
+	os.system("git clone https://github.com/thelinuxchoice/saycheese")
+
 os.system("clear")
-print(Fore.BLUE+"Загрузка прошла успешно, удачного хакинга (В ознакомительных целях конечно же(:)"+Style.RESET_ALL)
+print(Fore.BLUE+"Загрузка прошла успешно, удачного хакинга (В ознакомительных целях конечно же)"+Style.RESET_ALL)
