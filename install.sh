74eb124fadd1d7a71fe84b331b70400081039641
YanPek <54765921+YanPek@users.noreply.github.com>
2020-01-20 01:05:27 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#apt update && apt upgrade
-#pkg update && pkg upgrade
-#pip install --upgrade pip
-#pip install colorama
-scp Tsouls here
-scp Tsouls.py /data/data/com.termux/files/home/
-echo Загрузка прошла успешно
-echo Для запуска введите Tsouls
