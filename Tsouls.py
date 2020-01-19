318ebfd309460ae80fbba0e8a281dc26dfb0afbc
YanPek <54765921+YanPek@users.noreply.github.com>
2020-01-20 00:50:11 +0300

    it does not work, but still ahead
    it does not work, but still ahead

---
+++ Tsouls.py
@@ -0,0 +1,24 @@
+import os
+import colorama
+
+print(''' ________    _____     ____     __    __   _____        _____  
+(___  ___)  / ____\   / __ \    ) )  ( (  (_   _)      / ____\ 
+    ) )    ( (___    / /  \ \  ( (    ) )   | |       ( (___   
+   ( (      \___ \  ( ()  () )  ) )  ( (    | |        \___ \  
+    ) )         ) ) ( ()  () ) ( (    ) )   | |   __       ) ) 
+   ( (      ___/ /   \ \__/ /   ) \__/ (  __| |___) )  ___/ /  
+   /__\    /____/     \____/    \______/  \________/  /____/   
+                                                               
+                                                               
+                                          Выберите цифру:
+
+00) Сделать ваше устройство в хакинг машину
+
+01) Nmap (web сканирование) #apt install ngrok
+
+02) Sherlock (Поиск ников по разным соц-сетям #https://github.com/sherlock-project/sherlock.git
+
+03) Instagram + cupp (Брут паролей инстаграм + создание словаря) #git clone https://github.com/Pure-L0G1C/Instagram.git + git clone https://github.com/Mebus/cupp.git
+
+04)termux-ngrok (создание публичных url) #git clone https://github.com/tchelospy/termux-ngrok.git
+''') 
---
+++ install.sh
@@ -0,0 +1,8 @@
+#apt update && apt upgrade
+#pkg update && pkg upgrade
+#pip install --upgrade pip
+#pip install colorama
+scp Tsouls here
+scp Tsouls.py /data/data/com.termux/files/home/
+echo Загрузка прошла успешно
+echo Для запуска введите Tsouls
